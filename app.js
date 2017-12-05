994f947116cf907c9a1ffabce165807228063208
= <prianikovkirill@gmail.com>
2017-12-05 12:06:04 +0200

    Email works and checked

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mailer = require('express-mailer');
+var validator = require('validator');
 
 var app = express();
 
 mailer.extend(app, {
-    from: 'maushe4ka@gmail.com',
-    host: 'smtp.gmail.com', // hostname
-    secureConnection: true, // use SSL
-    port: 465, // port for secure SMTP
+    from: 'support@smart-itpro.com',
+    host: 'smtp.office365.com', // hostname
+    secureConnection: false, // use SSL
+    port: 587, // port for secure SMTP
     transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
     auth: {
-        user: 'maushe4ka@gmail.com',
-        pass: 'kos542910'
+        user: 'support@smart-itpro.com',
+        pass: 'X%lLm6%BSr5SPB'
     }
 });
 
@@ -33,21 +34,28 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/send-email', function (req, res) {
     var emailObj = req.body;
+    var editedObj = {};
 
-    console.log(emailObj);
+    for (var keyName in emailObj) {
+        editedObj[keyName] = validator.trim(emailObj[keyName]);
+        editedObj[keyName] = validator.escape(editedObj[keyName]);
+    }
+
+    if (!validator.isEmail(editedObj['email'])||validator.isEmpty(editedObj['personName'])||validator.isEmpty(editedObj['companyName'])) {
+        res.send({message: "Error! Fields did not pass validation on the server!", code: 500});
+        return;
+    }
 
     res.mailer.send('email', {
-        to: 'kostiantyn.koval@gmail.com', // REQUIRED. This can be a comma delimited string just like a normal email to field.
+        to: 'support@smart-itpro.com', // REQUIRED. This can be a comma delimited string just like a normal email to field.
         subject: 'Contact Message', // REQUIRED.
-        otherProperty: emailObj
+        otherProperty: editedObj
     }, function (err) {
         if (err) {
-            // handle error
-            console.error(err);
-            res.send('There was an error sending the email');
+            res.send({message: "Sorry, there was an error sending this email. Please try again later", code: 400, error: err});
             return;
         }
-        res.send('Email Sent');
+        res.send({message: "Thank you. Email was sent successfully", code: 200});
     });
 });
 
